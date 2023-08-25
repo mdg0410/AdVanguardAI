1fc6ccf17f05444b6e26ce24261436510936b898
mdg0410 <levoyersteven@gmail.com>
2023-08-25 16:04:28 -0500

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Provider } from "react-redux"
+import store from "./store"
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
@@ -6,9 +8,11 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider> 
   </React.StrictMode>
 );
 
